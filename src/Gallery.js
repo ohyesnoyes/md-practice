6663330d72d84dee6029c45044bc22ccb62a7f58
ohyesnoyes <brothernoyes@gmail.com>
2018-08-15 21:12:49 -0600

    edit gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,24 +4,26 @@ import React from 'react';
 class Gallery extends React.Component {
 
     render(){
-
+ 
+        // test array
+        // plan on an external json file with images and info
         const myArr = ['bacon', 'letuce', 'tomatoes'];
 
+        // loop thru the array and create li element for each image
         for (var i = 0; i < myArr.length; i++) {
             //create li for each element
             var li = document.createElement('li');
             var list = document.getElementById('list');
 
-            //add innerhtml to li elements
+            //add innerhtml to li elements to display
             let html = "<li>";
             html+= myArr[i];
             html+= "</li>";
-
-            //display li elements
             li.innerHTML = html;
             
             //this is throwing an error
             //kept sample list in the return as an example of what i want to do
+            // display the list items in the "list" ul
             list.appendChild(li);
             console.log(html);
         }
